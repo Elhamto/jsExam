9ac18d3fd93851470dfd5257a6d5c14b72fe2bde
Elhamto <elham.toloeii@gmail.com>
2022-08-27 18:30:59 +0430

    just up to k=4

--- q3.js
+++ q3.js
@@ -11,49 +11,69 @@ function maghsumha(num){
     return result;
 }
 // console.log(maghsumha(2));
-const set = []
 function product_sum(k) {
+    const set = new Array();
+    let lenSet = 0
     if (k<2) 
        throw "number must be bigger than 2"
-    const arr = new Array(k).fill(1);
     // arr=[2,4,1,1] 
-    let index = 2
+    let digit = 2
     let number = 1
-    while (index <= k) {
-        console.log("while1");
+    while (digit <= k) {
+        console.log("while1", digit);
+        const arr = new Array(digit).fill(1);
         let count = 1
-        while (count<= k) {
+        while (count< digit) {
             if (!isPrime(number)) {
-            console.log("while2", number);
-            
-                let maghsum = maghsumha(number).pop()
-                count++
-                // maghsum+(number/ maghsum)
-                if(maghsum+(number/ maghsum)==k-count){
-                    console.log(arr.pop().push(number));
-
-                }else{
-                    maghsumha(maghsum).pop()
-                    count++
-                    if(maghsum+(number/ maghsum)==k-count){
-                        console.log(arr.pop().push(number));
+                console.log("while2 if1", number);
+                const maghsumhaFunc =maghsumha(number)
+                for (let i = 0; i < maghsumhaFunc.length; i++) {  
+                    let maghsum = maghsumhaFunc.pop()  
+                    if (set.includes(number)) {
+                        maghsum =maghsumha(maghsum).pop()
+                        count++
+                        arr.shift()
+                        arr.push(maghsum)
                     }
+                    count++
+                    arr.shift()
+                    arr.push(maghsum)
+                    console.log(maghsum, count);
+                    let maghsumAlaih = number/maghsum
+                    arr.shift()
+                    arr.push(maghsumAlaih)
+                    const sum = arr.reduce(
+                        (previousValue, currentValue) => previousValue + currentValue, 0);      //sum elements of array
+                    const multiply = arr.reduce(
+                        (previousValue, currentValue) => previousValue * currentValue, 1);    //multiply elements of array
+                    if (sum === multiply) {
+                        set.push(number)
+                        console.log("if2",set);
+                        break
+                    }else{
+                        arr.pop()
+                        arr.pop()
+                        arr.push(1)
+                        arr.push(1)
+                    }
+                    console.log(arr);                    
                 }
             }
-            number++
+            // console.log("lenSet: ",lenSet," set.len: ",set.length,"-1:", set[set.length-1], "num: ",number);
+            if (lenSet ==set.length-1 && set[set.length-1]==number) {
+                number--
+                lenSet =set.length
+                break
+            }else{
+            number++}
         }
-        
-        index++
+        console.log("end W2",arr, number);
+        digit++
     }
-console.log(arr, number);
-    const sum = arr.reduce(
-        (previousValue, currentValue) => previousValue + currentValue, 0);      //sum elements of array
-    const multiply = arr.reduce(
-        (previousValue, currentValue) => previousValue * currentValue, 1);    //multiply elements of array
-    if (sum === multiply) {
-        set.push(multiply)
-    }    
-    // return [...new Set(set)]
+    console.log(set);
+    const result = [...new Set(set)].reduce(
+        (previousValue, currentValue) => previousValue + currentValue, 0);
+    return result
 }
-product_sum(2)
-// console.log(product_sum(6));
+// product_sum(2)
+console.log(product_sum(5));
